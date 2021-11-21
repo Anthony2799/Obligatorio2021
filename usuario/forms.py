7bb9338a0ab648c0c382ef0d686f7c524deaa193
DESKTOP-VCVOOCE\antho <anthony.johan@hotmail.com>
2021-11-21 19:18:19 -0300

    agrego alta, calculo yo

---
+++ usuario/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from django.db.models import fields
+from usuario.models import Entidad
+class usuarioForm(forms.ModelForm):
+    class Meta:
+        model = Entidad,
+        fields = '__all__'
--- usuario/views.py
+++ usuario/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect  
+from usuario.forms import usuarioForm    
+from usuario.models import Entidad  
+# Create usuario
+def crerUsuario(request):
+    if request.method == "POST":
+        form = usuarioForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/show')
+            except:
+                pass
+        else:
+            form = usuarioForm()
+            return render(request, 'index.html',{'form':form})
 
-# Create your views here.
+def show(request):
+    usuarios = Entidad.objects.all()
+    return render(request,"show.html",{'usuarios':usuarios})
