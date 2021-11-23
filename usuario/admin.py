f01792503dad1388ff274b9f870c326da0481e30
jose <jose@rpamaker.com>
2021-11-23 00:56:21 -0300

    registro Clientes

--- usuario/admin.py
+++ usuario/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Perfil_cliente
 # Register your models here.
+
+@admin.register(Perfil_cliente)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('entidad_usuario','nombre_usuario','apellido_usuario','documento_usuario','fecha_creado','fecha_modficado')
+    search_fields = ('nombre_usuario','documento_usuario')
+
