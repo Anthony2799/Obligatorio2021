696a9ac38cc3ab8cf93a6ed5fb66fbc488a19d61
jose <jose@rpamaker.com>
2021-11-24 21:45:46 -0300

    incluir Map

--- static/js/mpas.js
+++ static/js/mpas.js
@@ -1,55 +1,47 @@
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
 
           
-          var strArray = [];
-   
-          var draggablePolygon; function InitMap() {
-              var location = new google.maps.LatLng(39.86232, -4.0694706);
-              var mapOptions = {
-                  zoom: 7,
-                  center: location,
-                  mapTypeId: google.maps.MapTypeId.RoadMap
-              };
-  
-  
-              var map = new google.maps.Map(document.getElementById('map-canvas'),
-                  mapOptions);
-  
-              var shapeCoordinates = [
-  
-                  new google.maps.LatLng(39.86232, -4.0694706),
-                  new google.maps.LatLng(39.86232, -4.0694706)
-              ];
-              // Construct the polygon
-              draggablePolygon = new google.maps.Polygon({
-                  paths: shapeCoordinates,
-                  draggable: true,
-                  editable: true,
-                  strokeColor: '',
-                  strokeOpacity: 0.8,
-                  strokeWeight: 2,
-                  fillColor: '#ADFF2F',
-                  fillOpacity: 0.5
-              });
-              draggablePolygon.setMap(map);
-              google.maps.event.addListener(draggablePolygon, "dragend", Getpolygoncoordinates);
-              google.maps.event.addListener(draggablePolygon.getPath(), "insert_at", Getpolygoncoordinates);
-              google.maps.event.addListener(draggablePolygon.getPath(), "remove_at", Getpolygoncoordinates);
-              google.maps.event.addListener(draggablePolygon.getPath(), "set_at", Getpolygoncoordinates);
-          }
-  
-          function Getpolygoncoordinates() {
-              var len = draggablePolygon.getPath().getLength();
-              strArray = [];
-              for (var i = 0; i < len; i++) {
-                  strArray[strArray.length] = { Latitud: draggablePolygon.getPath().getAt(i).toUrlValue(5).split(",")[0], Longitud: draggablePolygon.getPath().getAt(i).toUrlValue(5).split(",")[1] };
-              }
-  
-          }
+var strArray = [];
+
+var draggablePolygon; function InitMap() {
+    var location = new google.maps.LatLng(39.86232, -4.0694706);
+    var mapOptions = {
+        zoom: 7,
+        center: location,
+        mapTypeId: google.maps.MapTypeId.RoadMap
+    };
+
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+    var shapeCoordinates = [
+
+        new google.maps.LatLng(39.86232, -4.0694706),
+        new google.maps.LatLng(39.86232, -4.0694706)
+    ];
+    // Construct the polygon
+    draggablePolygon = new google.maps.Polygon({
+        paths: shapeCoordinates,
+        draggable: true,
+        editable: true,
+        strokeColor: '',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#ADFF2F',
+        fillOpacity: 0.5
+    });
+    draggablePolygon.setMap(map);
+    google.maps.event.addListener(draggablePolygon, "dragend", Getpolygoncoordinates);
+    google.maps.event.addListener(draggablePolygon.getPath(), "insert_at", Getpolygoncoordinates);
+    google.maps.event.addListener(draggablePolygon.getPath(), "remove_at", Getpolygoncoordinates);
+    google.maps.event.addListener(draggablePolygon.getPath(), "set_at", Getpolygoncoordinates);
+}
+
+function Getpolygoncoordinates() {
+    var len = draggablePolygon.getPath().getLength();
+    strArray = [];
+    for (var i = 0; i < len; i++) {
+        strArray[strArray.length] = { Latitud: draggablePolygon.getPath().getAt(i).toUrlValue(5).split(",")[0], Longitud: draggablePolygon.getPath().getAt(i).toUrlValue(5).split(",")[1] };
+    }
+
+}
