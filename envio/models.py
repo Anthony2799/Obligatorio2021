5011cf45d83491e3589925e5db2a16bdf5f879de
jose <jose@rpamaker.com>
2021-12-03 18:40:05 -0300

    .

--- envio/models.py
+++ envio/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class Envio(models.Model):
-    costo = models.FloatField(blank=False,null=False)
-    estado = models.CharField(max_length=20)
-    peso_paquete = models.FloatField(blank=False,null=False)
-    numero_entidad = models.ForeignKey("usuario.Entidad",verbose_name='numero_entidad', on_delete=models.CASCADE)
-    documento_usuario = models.ForeignKey("usuario.Perfil_funcionario",verbose_name='documento_funcionario', on_delete=models.CASCADE)
-    Latitud = models.CharField(max_length=20 , blank=True)
-    Longitud = models.CharField(max_length=20 , blank=True)
-    #numero_zona =models.ForeignKey("users.Perfil_funcionario", on_delete=models.CASCADE)
 
-    
+class Envio(models.Model):
+    costo = models.FloatField(blank=False, null=False)
+    estado = models.CharField(max_length=20)
+    peso_paquete = models.FloatField(blank=False, null=False)
+    numero_entidad = models.ForeignKey("usuario.Entidad", verbose_name='numero_entidad', on_delete=models.CASCADE)
+    documento_usuario = models.ForeignKey("usuario.Perfil_funcionario", verbose_name='documento_funcionario',
+                                          on_delete=models.CASCADE)
+    Latitud = models.CharField(max_length=20, blank=True)
+    Longitud = models.CharField(max_length=20, blank=True)
+    # numero_zona =models.ForeignKey("users.Perfil_funcionario", on_delete=models.CASCADE)
--- mapa/models.py
+++ mapa/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 
+
 # Create your models here.
-
-
 
 
 class Zona(models.Model):
@@ -13,11 +12,11 @@ class Zona(models.Model):
 class Ubicacion_zona(models.Model):
     latitud = models.CharField(max_length=50, blank=False)
     longitud = models.CharField(max_length=50, blank=False)
-    nro_zona = models.ForeignKey('mapa.Zona', verbose_name='id', on_delete=CASCADE,blank=False)
+    nro_zona = models.ForeignKey('mapa.Zona', verbose_name='id', on_delete=CASCADE, blank=False)
 
 
 class Vehiculo_distribuidora(models.Model):
     matricula = models.CharField(max_length=20, primary_key=True)
-    nro_zona = models.ForeignKey("mapa.Zona", verbose_name= 'id', on_delete=CASCADE, blank=False)
+    nro_zona = models.ForeignKey("mapa.Zona", verbose_name='id', on_delete=CASCADE, blank=False)
     estado_vehiculo = models.CharField(max_length=20, blank=False)
     capacidad = models.FloatField(blank=False)
